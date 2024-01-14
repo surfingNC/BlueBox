bab224afa4721fe547d2bff605c7f71b16c9db21
surfingNC <hansendavid46@gmail.com>
2024-01-13 21:35:37 -0500

    added logo image

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ImageButton from "./components/ImageButton";
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <div>
+      <ImageButton></ImageButton>
       {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+        <Alert onClose={() => setAlertVisibility(false)}>
+          One day, I will make money programming
+        </Alert>
       )}
       <Button color="primary" onClick={() => setAlertVisibility(true)}>
         My Button
---
+++ src/components/ImageButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import img1 from "./images/bluesBoxLogo.png";
+
+const ImageButton = () => {
+  return (
+    <div>
+      <img src={img1} alt="" />
+    </div>
+  );
+};
+
+export default ImageButton;
